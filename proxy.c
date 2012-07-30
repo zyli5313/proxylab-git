5dd56c50d175818f1605dd9d77cab869941217fb
zyli <zyli5313@gmail.com>
2012-07-30 18:57:39 +0800

    first commit proxylab

---
+++ proxy.c
@@ -0,0 +1,31 @@
+/*
+ * proxy.c - A cacheing web proxy
+ * Andrew ID1: zeyuanl
+ * Andrew ID2: yiwench
+ */
+
+#include <stdio.h>
+#include "csapp.h"
+
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+static const char *user_agent = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *accept = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
+static const char *accept_encoding = "Accept-Encoding: gzip, deflate\r\n";
+
+void doit(int fd);
+void read_requesthdrs(rio_t *rp);
+int parse_uri(char *uri, char *filename, char *cgiargs);
+void serve_static(int fd, char *filename, int filesize);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filename, char *cgiargs);
+void clienterror(int fd, char *cause, char *errnum,
+                 char *shortmsg, char *longmsg);
+
+int main()
+{
+    printf("%s%s%s", user_agent, accept, accept_encoding);
+
+    return 0;
+}
